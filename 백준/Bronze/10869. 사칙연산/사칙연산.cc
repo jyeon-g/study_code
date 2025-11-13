ab1b69ecc5a9797ff09dee2c0eca7d625d1f3b45
JiYeon <qkrwldus8@gmail.com>
2025-11-13 16:56:48 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int A, B;
+
+	std::cin >> A >> B;
+	std::cout << A + B << std::endl;
+	std::cout << A - B << std::endl;
+	std::cout << A * B << std::endl;
+	std::cout << A / B << std::endl;
+	std::cout << A % B << std::endl;
+	return 0;
+}
